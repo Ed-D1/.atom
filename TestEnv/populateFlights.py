627af3f7697f99f8b5a89b48817e273e69bee7dd
Eddie <Eddie943@outlook.com>
2022-11-12 10:54:07 -0600

    practice

--- TestEnv/populateFlights.py
+++ TestEnv/populateFlights.py
@@ -1,2 +1,10 @@
 import gen_customer
-import Plane
+import Plane as Pl
+import random
+
+for aLine in Pl.SkyAir.airlines:
+    for flight in aLine.flights:
+        start = random.getrandint(0, 24)
+        finish = random.getrandint(0, 24)
+        
+        flight.changeDest(start, finish)
